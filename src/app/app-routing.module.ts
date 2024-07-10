a432091b32166ebd78000af6256cac42012b773f
LeSage1945 <koukodilaexauce636@gmail.com>
2024-07-10 13:25:23 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EleveComponent } from './eleve/eleve.component';
+import { ActionEleveComponent } from './eleve/action-eleve/action-eleve.component';
+import { AgentComponent } from './agent/agent.component';
+import { ActionFamilleComponent } from './eleve/famille/action-famille/action-famille.component';
+
+const routes: Routes = [
+  {path: 'eleve', component: EleveComponent},
+  {path: 'agent', component: AgentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
