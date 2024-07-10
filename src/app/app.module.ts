26b7397f789fbe903910d1671ce4c4df545e4bf0
LeSage1945 <koukodilaexauce636@gmail.com>
2024-07-10 12:19:02 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EleveComponent } from './eleve/eleve.component';
+import { AgentComponent } from './agent/agent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EleveComponent,
+    AgentComponent
   ],
   imports: [
     BrowserModule,
